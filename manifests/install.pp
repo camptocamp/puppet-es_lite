ea5d7a96d0ea2658e5cba6bf74d53c48797f8811
Mickaël Canévet <mickael.canevet@camptocamp.com>
2019-10-30 17:41:37 +0100

    Deploy ES using package

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,4 +5,29 @@
 # @example
 #   include es_lite::install
 class es_lite::install {
+  case $::es_lite::install_method {
+    'package': {
+      ensure_packages('wget')
+      exec { 'import-elasticsearch-7.x-key':
+        path      => '/bin:/usr/bin:/sbin:/usr/sbin',
+        command   => 'rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+        logoutput => 'on_failure',
+        unless    => "rpm -q gpg-pubkey-$(echo $(wget -q https://artifacts.elastic.co/GPG-KEY-elasticsearch -O - | gpg -q --throw-keyids --keyid-format short | grep pub | cut -f2 -d/ | cut -f1 -d' ' | tr '[A-Z]' '[a-z]'))"
+      }
+      -> yumrepo { 'elasticsearch':
+        name     => 'elasticsearch-7.x',
+        descr    => 'Elasticsearch repository for 7.x packages',
+        baseurl  => 'https://artifacts.elastic.co/packages/7.x/yum',
+        gpgcheck => 1,
+        gpgkey   => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+        enabled  => 1,
+      }
+      -> package { 'elasticsearch':
+        ensure => $es_lite::version,
+      }
+    }
+    default: {
+      fail "Unsupported install method: ${::es_lite::install_method}"
+    }
+  }
 }
