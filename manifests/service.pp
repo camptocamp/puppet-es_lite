5130aace9099f36e8d6b5b5d9ffca8ae982d3017
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2019-11-12 18:32:06 +0100

    Allow Elasticsearch to lock memory at startup

--- manifests/service.pp
+++ manifests/service.pp
@@ -5,6 +5,15 @@
 # @example
 #   include es_lite::service
 class es_lite::service {
+  include systemd::systemctl::daemon_reload
+
+  systemd::dropin_file { 'elasticsearch.conf':
+    unit   => 'elasticsearch.service',
+    source => 'puppet:///modules/es_lite/systemd-service-override.conf',
+  }
+
+  Class['systemd::systemctl::daemon_reload']->Service['elasticsearch']
+
   $ensure = $es_lite::manage_service ? {
     true  => 'running',
     false => undef,
