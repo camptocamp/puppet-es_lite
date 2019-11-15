1caa5ee56ab095a107efb84ce3a7219195153f71
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2019-11-15 13:57:04 +0100

    Fix service restarting even if unmanaged

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,6 +11,17 @@ class es_lite(
   Boolean                   $manage_service = false,
 ) {
   class { '::es_lite::install': }
-  -> class { '::es_lite::config': }
-  ~> class { '::es_lite::service': }
+
+  $notify = $es_lite::manage_service ? {
+      true  => Class['::es_lite::service'],
+      false => undef,
+    }
+  class { '::es_lite::config':
+    require => Class['::es_lite::install'],
+    notify  => $notify,
+  }
+
+  class { '::es_lite::service':
+    require => Class['::es_lite::config'],
+  }
 }
