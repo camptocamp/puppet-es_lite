4b1d875ce0572709fec59c844e15b43a22ecfe9e
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2019-11-19 12:54:26 +0100

    Allow installing various Elasticsearch major versions

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,22 +8,29 @@ class es_lite::install {
   case $::es_lite::install_method {
     'package': {
       ensure_packages('wget')
-      exec { 'import-elasticsearch-7.x-key':
+      exec { 'import-elasticsearch-key':
         path      => '/bin:/usr/bin:/sbin:/usr/sbin',
         command   => 'rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch',
         logoutput => 'on_failure',
         unless    => "rpm -q gpg-pubkey-$(echo $(wget -q https://artifacts.elastic.co/GPG-KEY-elasticsearch -O - | gpg -q --throw-keyids --keyid-format short | grep pub | cut -f2 -d/ | cut -f1 -d' ' | tr '[A-Z]' '[a-z]'))"
       }
-      -> yumrepo { 'elasticsearch':
-        name     => 'elasticsearch-7.x',
-        descr    => 'Elasticsearch repository for 7.x packages',
-        baseurl  => 'https://artifacts.elastic.co/packages/7.x/yum',
+      if ($::es_lite::version =~ /^([0-9]+)\.[0-9]+\.[0-9]+$/) {
+        $major_version = $1
+      } else {
+        $major_version = 7
+      }
+      yumrepo { 'elasticsearch':
+        name     => "elasticsearch-${major_version}.x",
+        descr    => "Elasticsearch repository for ${major_version}.x packages",
+        baseurl  => "https://artifacts.elastic.co/packages/${major_version}.x/yum",
         gpgcheck => 1,
         gpgkey   => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
         enabled  => 1,
+        require  => Exec['import-elasticsearch-key'],
       }
-      -> package { 'elasticsearch':
-        ensure => $es_lite::version,
+      package { 'elasticsearch':
+        ensure  => $es_lite::version,
+        require => Yumrepo['elasticsearch'],
       }
     }
     default: {
